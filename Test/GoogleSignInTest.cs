620f311e4e3b3088d99fc8832a9a496c53575ba3
HanzaRu <john.rosa@aquiris.com.br>
2020-10-28 22:05:18 -0300

    Tests

--- Test/GoogleSignInTest.cs
+++ Test/GoogleSignInTest.cs
@@ -1,18 +1,62 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
-public class GoogleSignInTest : MonoBehaviour
+namespace Google.Tests
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GoogleSignInTest : MonoBehaviour
     {
-        
-    }
+        public string WebClientId;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private GoogleSignInConfiguration GetConfiguration()
+        {
+            return new GoogleSignInConfiguration { WebClientId = WebClientId, RequestIdToken = true };
+        }
+
+        public void SignIn()
+        {
+            GoogleSignIn.Configuration = GetConfiguration();
+            GoogleSignIn.DefaultInstance.SignIn().ContinueWith(OnAuthenticationFinished);
+        }
+
+        public void SignOut()
+        {
+            GoogleSignIn.DefaultInstance.SignOut();
+        }
+
+        public void SignInSilently()
+        {
+            GoogleSignIn.Configuration = GetConfiguration();
+            GoogleSignIn.DefaultInstance.SignInSilently().ContinueWith(OnAuthenticationFinished);
+        }
+
+        private void OnAuthenticationFinished(Task<GoogleSignInUser> task)
+        {
+            if (task.IsFaulted)
+            {
+                using (IEnumerator<Exception> enumerator = task.Exception.InnerExceptions.GetEnumerator())
+                {
+                    if (enumerator.MoveNext())
+                    {
+                        GoogleSignIn.SignInException error = (GoogleSignIn.SignInException)enumerator.Current;
+                        Debug.LogError("Got Error: " + error.Status + " " + error.Message);
+                    }
+                    else
+                    {
+                        Debug.LogError("Got Unexpected Exception?!?" + task.Exception);
+                    }
+                }
+                return;
+            }
+
+            if (task.IsCanceled)
+            {
+                Debug.LogWarning("SignIn cancelled.");
+                return;
+            }
+
+            Debug.Log("SignedIn: welcome " + task.Result.DisplayName + "!");
+        }
     }
 }
